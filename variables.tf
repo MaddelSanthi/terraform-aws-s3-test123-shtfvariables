e88da9f7ddf7ef7bef0ae716dbff810cd64ecade
MaddelSanthi <168314503+MaddelSanthi@users.noreply.github.com>
2024-06-20 20:21:08 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "unique_id" {
+  description = "A unique identifier for the bucket name"
+  type        = string
+  #default     = "20240618-1234"  # Replace with your unique identifier
+}
+
